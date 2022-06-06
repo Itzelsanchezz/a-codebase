3f96b4783185890b0a174b91fe7c2dcbb283e384
Itzelsanchezz <72948547+Itzelsanchezz@users.noreply.github.com>
2022-06-06 16:40:44 -0500

    worked on components and state variables

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./styles.css";
 
 export default function App() {
   const [playerTurn, changePlayerTurn] = useState("red");
-  const [column, changeColumn] = useState([]);
+  const [column, changeColumn] = useState(['red','yellow',]);
 
   function dropSlotInColumn() {
     if (column.length < 6) {
@@ -21,8 +21,9 @@ export default function App() {
     }
   }
 
-  return (
-    <div className="App">
+ 
+  function Column(){
+    return (
       <div className="blue" onClick={dropSlotInColumn}>
         <div className={`slot ${column[5]}`}></div>
         <div className={`slot ${column[4]}`}></div>
@@ -30,8 +31,19 @@ export default function App() {
         <div className={`slot ${column[2]} `}></div>
         <div className={`slot ${column[1]}`}></div>
         <div className={`slot ${column[0]}`}></div>
-      </div>{" "}
+      </div>
+    )
+  }
+
+  
+ const board,updateBoard = useState([[], [], [], [], [], [], []]);
+
+  return (
+    <div className="App">
+      <Column/>
+      <Column/>
       It is {playerTurn}'s turn{" "}
     </div>
   );
 }
+
